f1b8dfcde1c3f86117828722354503509ed5f4f0
大北悟 <satoru.ohkita@gmail.com>
2023-01-26 11:55:44 +0900

    Created index.blade.php in the tweet directory and displayed HTML from the controller.

--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -15,6 +15,7 @@ class IndexController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return 'Single Action!';
+        // return 'Single Action!';
+        return view('tweet.index', ['name' => 'Laravel(Tweet/IndexControllerのviewヘルパー関数に記述)']);
     }
 }
