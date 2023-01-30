10d4662afa62d0a77e8ed8803d69a5f9daaf311f
大北悟 <satoru.ohkita@gmail.com>
2023-01-30 16:51:08 +0900

    キャッシュバスティングを実現(テキストp149)

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -7,7 +7,6 @@
         content="width=device-width, user-scalable=no, initial-scale=1.0 maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>つぶやきアプリ</title>
-    <link rel="stylesheet" href="/css/app.css">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <script src="{{ mix('js/app.js') }}" defer></script>
 </head>
