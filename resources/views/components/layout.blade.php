c39581af0237c6d3dd0d067254a8ee375cda0a0b
大北悟 <satoru.ohkita@gmail.com>
2023-01-30 23:40:44 +0900

    クラスベースコンポーメントを作成（p162~167）

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,6 +9,7 @@
     <title>{{ $title ?? 'つぶやきアプリ' }}</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <script src="{{ mix('js/app.js') }}"></script>
+    @stack('css')
 </head>
 
 <body class="bg-gray-50">
